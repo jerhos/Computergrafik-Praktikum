fc0c5a0ac48b9c557c0456821d9c7befc2d65799
jerhos <91937785+jerhos@users.noreply.github.com>
2022-01-21 23:05:47 +0100

    did Tut11

--- Tut11_AssetsPicking/Tut11_AssetsPicking.cs
+++ Tut11_AssetsPicking/Tut11_AssetsPicking.cs
@@ -21,39 +21,47 @@ namespace FuseeApp
     public class Tut11_AssetsPicking : RenderCanvas
     {
         private SceneContainer _scene;
+        private ScenePicker _scenePicker;
         private SceneRendererForward _sceneRenderer;
+        private PickResult _currentPick;
+        private float4 _oldColor;
+        
+        
+        //---Transforms---\\
+        // Body (Wall-E)
         private Transform _baseTransform;
+        // Wheels
+        private Transform _rightRearTransform;
+        private Transform _rightFrontTransform;
+        private Transform _rightUpperTransform;
+        private Transform _leftRearTransform;
+        private Transform _leftFrontTransform;
+        private Transform _leftUpperTransform;
+        // Neck
+        private Transform _lowerNeck;
+        private Transform _upperNeck;
+        // Head
+        private Transform _head;
 
         SceneContainer CreateScene()
         {
             // Initialize transform components that need to be changed inside "RenderAFrame"
-            _baseTransform = new Transform
-            {
-                Rotation = new float3(0, 0, 0),
-                Scale = new float3(1, 1, 1),
-                Translation = new float3(0, 0, 0)
-            };
+            InitializeTransform(_baseTransform      );
+            InitializeTransform(_rightRearTransform );
+            InitializeTransform(_rightFrontTransform);
+            InitializeTransform(_rightUpperTransform);
+            InitializeTransform(_leftRearTransform  );
+            InitializeTransform(_leftFrontTransform );
+            InitializeTransform(_leftUpperTransform );
+            InitializeTransform(_lowerNeck          );
+            InitializeTransform(_upperNeck          );
+            InitializeTransform(_head               );
 
             // Setup the scene graph
             return new SceneContainer
             {
                 Children = new List<SceneNode>
-                {
-                    new SceneNode
-                    {
-                        Components = new List<SceneComponent>
-                        {
-                            // TRANSFROM COMPONENT
-                            _baseTransform,
-
-                            // SHADER EFFECT COMPONENT
-                            SimpleMeshes.MakeMaterial((float4) ColorUint.LightGrey),
-
-                            // MESH COMPONENT
-                            // SimpleAssetsPickinges.CreateCuboid(new float3(10, 10, 10))
-                            SimpleMeshes.CreateCuboid(new float3(10, 10, 10))
-                        }
-                    },
+                { 
                 }
             };
         }
@@ -61,9 +69,21 @@ namespace FuseeApp
         // Init is called on startup. 
         public override void Init()
         {
-            RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
+            RC.ClearColor = new float4(0, 0, 0.1f, 1);
 
-            _scene = CreateScene();
+            _scene = AssetStorage.Get<SceneContainer>("WALL-E_aufWishBestellt.fus"); // Bei meinem ursprünglichen Modell (WALL-E.fus) wird die Farbe anstatt des Objektes ausgewählt
+            _scenePicker = new ScenePicker(_scene);
+
+            _baseTransform          = GetTransformOf("Wall-E");
+            _rightRearTransform     = GetTransformOf("rightRearWheel");
+            _rightFrontTransform    = GetTransformOf("rightFrontWheel");
+            _rightUpperTransform    = GetTransformOf("rightUpperWheel");
+            _leftRearTransform      = GetTransformOf("leftRearWheel");
+            _leftFrontTransform     = GetTransformOf("leftFrontWheel");
+            _leftUpperTransform     = GetTransformOf("leftUpperWheel");
+            _lowerNeck              = GetTransformOf("neck1");
+            _upperNeck              = GetTransformOf("neck2");
+            _head                   = GetTransformOf("head");
 
             // Create a scene renderer holding the scene above
             _sceneRenderer = new SceneRendererForward(_scene);
@@ -83,10 +103,38 @@ namespace FuseeApp
 
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
-
+            
             // Setup the camera 
             RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float) Math.Atan(15.0 / 40.0));
 
+            
+            if (Mouse.LeftButton)
+            {
+                float2 pickPosClip = Mouse.Position * new float2(2.0f / Width, -2.0f / Height) + new float2(-1, 1);
+          
+                PickResult newPick = _scenePicker.Pick(RC, pickPosClip).OrderBy(pr => pr.ClipPos.z).FirstOrDefault();
+          
+                 if (newPick?.Node != _currentPick?.Node)
+                 {
+                     if (_currentPick != null)
+                     {
+                         var ef = _currentPick.Node.GetComponent<SurfaceEffect>();
+                         ef.SurfaceInput.Albedo = _oldColor;
+                     }
+                     if (newPick != null)
+                     {
+                         var ef = newPick.Node.GetComponent<SurfaceEffect>();
+                         _oldColor = ef.SurfaceInput.Albedo;
+                         ef.SurfaceInput.Albedo = (float4) ColorUint.White;
+                         Diagnostics.Debug($"Object {newPick.Node.Name} picked.");
+                     }
+                     _currentPick = newPick;
+                  }
+            }
+            if (_currentPick != null)
+            {
+            setRotationOfCurrent();
+            }
             // Render the scene on the current render context
             _sceneRenderer.Render(RC);
 
@@ -107,6 +155,72 @@ namespace FuseeApp
             // Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
             var projection = float4x4.CreatePerspectiveFieldOfView(M.PiOver4, aspectRatio, 1, 20000);
             RC.Projection = projection;
-        }                
+        }  
+
+        public Transform GetTransformOf (string name) 
+        {
+            return _scene.Children.FindNodes(node => node.Name == name)?.FirstOrDefault()?.GetTransform();
+        }   
+
+        public void InitializeTransform (Transform t) 
+        {
+            t = new Transform
+            {
+                Rotation    = new float3(0, 0, 0),
+                Scale       = new float3(1, 1, 1),
+                Translation = new float3(0, 0, 0)
+            };
+        }
+
+        public void setRotationOfCurrent()
+        {
+                float speed = 0.1f;
+                switch (_currentPick.Node.Name)
+                {
+                    case "Wall-E":
+                        _currentPick.Node.GetTransform().Rotation += new float3(-Keyboard.UpDownAxis * (speed * 10), Keyboard.ADAxis * (speed * 20), Keyboard.LeftRightAxis * (speed * 10));
+                        Diagnostics.Debug(_currentPick.Node.GetTransform().Rotation);
+                    break;
+                    case "rightRearWheel":
+                        _currentPick.Node.GetTransform().Rotation += new float3(-Keyboard.UpDownAxis * speed, 0, 0);
+                        Diagnostics.Debug(_currentPick.Node.GetTransform().Rotation);
+                    break;
+                    case "rightFrontWheel":
+                        _currentPick.Node.GetTransform().Rotation += new float3(-Keyboard.UpDownAxis * speed, 0, 0);
+                        Diagnostics.Debug(_currentPick.Node.GetTransform().Rotation);
+                    break;
+                    case "rightUpperWheel":
+                        _currentPick.Node.GetTransform().Rotation += new float3(-Keyboard.UpDownAxis * speed, 0, 0);
+                        Diagnostics.Debug(_currentPick.Node.GetTransform().Rotation);
+                    break;
+                    case "leftRearWheel":
+                        _currentPick.Node.GetTransform().Rotation += new float3(-Keyboard.UpDownAxis * speed, 0, 0);
+                        Diagnostics.Debug(_currentPick.Node.GetTransform().Rotation);
+                    break;
+                    case "leftFrontWheel":
+                        _currentPick.Node.GetTransform().Rotation += new float3(-Keyboard.UpDownAxis * speed, 0, 0);
+                        Diagnostics.Debug(_currentPick.Node.GetTransform().Rotation);
+                    break;
+                    case "leftUpperWheel":
+                        _currentPick.Node.GetTransform().Rotation += new float3(-Keyboard.UpDownAxis * speed, 0, 0);
+                        Diagnostics.Debug(_currentPick.Node.GetTransform().Rotation);
+                    break;
+                    case "neck1":
+                        _currentPick.Node.Parent.GetTransform().Rotation += new float3(-Keyboard.UpDownAxis * speed, Keyboard.ADAxis * speed, Keyboard.LeftRightAxis * speed);
+                        Diagnostics.Debug(_currentPick.Node.Parent.GetTransform().Rotation);
+                    break;
+                    case "neck2":
+                        _currentPick.Node.Parent.GetTransform().Rotation += new float3(-Keyboard.UpDownAxis * speed, Keyboard.ADAxis * speed, Keyboard.LeftRightAxis * speed);
+                        Diagnostics.Debug(_currentPick.Node.Parent.GetTransform().Rotation);
+                    break;
+                    case "head":
+                        _currentPick.Node.GetTransform().Rotation += new float3(0, Keyboard.ADAxis * speed, 0);
+                        Diagnostics.Debug(_currentPick.Node.GetTransform().Rotation);
+                    break;
+                    default:
+                        Diagnostics.Debug("Found no scenenode as current pick");
+                    break;
+            }
+        }        
     }
 }
